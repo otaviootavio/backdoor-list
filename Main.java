e6bde11d62a36cf18311192d9513476bd019858e
Otavio Martins <otaviovacari@usp.br>
2023-03-22 17:08:38 -0300

    Cli UI + Payload

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -7,34 +8,49 @@ import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.Arrays;
 import java.util.Base64;
 
 /**
  * main
  */
 public class Main {
-    public static final String PASSWORD = "1234";
-    public static final String PASSWORD_SHA3 = "1d6442ddcfd9db1ff81df77cbefcd5afcc8c7ca952ab3101ede17a84b866d3f3";
-    public static final String PASSWORD_BINARY = "00110001001100100011001100110100";
+    public static final char[] MASTERKEY = "1234".toCharArray();
+    public static final char[] MASTERKEY_SHA3 = "1d6442ddcfd9db1ff81df77cbefcd5afcc8c7ca952ab3101ede17a84b866d3f3"
+            .toCharArray();
+    public static final char[] MASTERKEY_BINARY = "10011010010".toCharArray();
+    public static final char[] MASTERKEY_FLIP_BINARY = "11001110110011011100110011001011".toCharArray();
 
     public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        // Binary
-        String binaryPassword = convertStringToBinary(PASSWORD);
 
-        // FlippingBits
-        String flippedBinaryPassword = convertStringToFlippedBinary(PASSWORD);
+        Console console = System.console();
+        console.printf("Username: ");
+        String username = console.readLine();
 
-        // To sha3
+        console.printf("Password: ");
+        char[] pass = console.readPassword();
+
+        // SHA3 Conversion
         final MessageDigest digest = MessageDigest.getInstance("SHA3-256");
-        final byte[] hashbytes = digest.digest(PASSWORD.getBytes(StandardCharsets.UTF_8));
-        String sha3Hex = (String) bytesToHex(hashbytes);
+        final byte[] hashbytes = digest.digest(String.valueOf(pass).getBytes(StandardCharsets.UTF_8));
+        char[] pass_sha3 = ((String) bytesToHex(hashbytes)).toCharArray();
+        /// End of conversion
 
-        System.out.println(PASSWORD);
-        System.out.println(binaryPassword);
-        System.out.println(flippedBinaryPassword);
-        System.out.println(sha3Hex);
+        if (Arrays.equals(pass, MASTERKEY)) {
+            System.out.println("Backdoor 1");
+        }
 
-        PayloadObfuscated();
+        if (Arrays.equals(pass_sha3, MASTERKEY_SHA3)) {
+            System.out.println("Backdoor 2");
+        }
+
+        if (Arrays.equals(convertStringToBinary(pass), MASTERKEY_BINARY)) {
+            System.out.println("Backdoor 3");
+        }
+
+        if (Arrays.equals(convertStringToFlippedBinary(pass), MASTERKEY_FLIP_BINARY)) {
+            System.out.println("Backdoor 4");
+        }
     }
 
     private static String bytesToHex(byte[] hash) {
@@ -49,36 +65,36 @@ public class Main {
         return hexString.toString();
     }
 
-    private static String convertStringToBinary(String input) {
+    private static char[] convertStringToBinary(char[] input) {
 
         StringBuilder result = new StringBuilder();
-        char[] chars = input.toCharArray();
+        char[] chars = input;
         for (char aChar : chars) {
             result.append(
                     String.format("%8s", Integer.toBinaryString(aChar)) // char -> int, auto-cast
                             .replaceAll(" ", "0") // zero pads
             );
         }
-        return result.toString();
+        return result.toString().toCharArray();
     }
 
-    private static String convertStringToFlippedBinary(String input) {
+    private static char[] convertStringToFlippedBinary(char[] input) {
 
         StringBuilder result = new StringBuilder();
-        char[] chars = input.toCharArray();
+        char[] chars = input;
         for (char aChar : chars) {
             result.append(
                     String.format("%8s", Integer.toBinaryString(aChar)) // char -> int, auto-cast
                             .replaceAll(" ", "0") // zero pads
             );
         }
-        return result.toString().replace("0", "x").replace("1", "0").replace("x", "1");
+        return result.toString().replace("0", "x").replace("1", "0").replace("x", "1").toCharArray();
     }
 
     /**
      * Just a backdoor payload
      */
-    private static void Payload() {
+    private static void PayloadUsesProcessBuilder() {
 
         try {
             boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
@@ -110,7 +126,7 @@ public class Main {
         }
     }
 
-    private static void PayloadObfuscated() throws UnsupportedEncodingException {
+    private static void PayloadObfuscatedUsesProcessBuilder() throws UnsupportedEncodingException {
         try {
 
             byte[] cmd_exe_base64 = "Y21kLmV4ZQo".getBytes("UTF-8");
@@ -118,8 +134,7 @@ public class Main {
             byte[] dir_base64 = "ZGly".getBytes("UTF-8");
             byte[] sh_base64 = "c2g".getBytes("UTF-8");
             byte[] c_base64 = "LWM".getBytes("UTF-8");
-            byte[] ls_base64 = "bHM".getBytes("UTF-8");    
-        
+            byte[] ls_base64 = "bHM".getBytes("UTF-8");
 
             boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
 
@@ -127,16 +142,14 @@ public class Main {
 
             if (isWindows) {
                 builder.command(
-                    new String(Base64.getDecoder().decode(cmd_exe_base64), StandardCharsets.UTF_8),
-                    new String(Base64.getDecoder().decode(slash_c_base64), StandardCharsets.UTF_8),
-                    new String(Base64.getDecoder().decode(dir_base64), StandardCharsets.UTF_8)
-                );
+                        new String(Base64.getDecoder().decode(cmd_exe_base64), StandardCharsets.UTF_8),
+                        new String(Base64.getDecoder().decode(slash_c_base64), StandardCharsets.UTF_8),
+                        new String(Base64.getDecoder().decode(dir_base64), StandardCharsets.UTF_8));
             } else {
                 builder.command(
-                    new String(Base64.getDecoder().decode(sh_base64), StandardCharsets.UTF_8),
-                    new String(Base64.getDecoder().decode(c_base64), StandardCharsets.UTF_8),
-                    new String(Base64.getDecoder().decode(ls_base64), StandardCharsets.UTF_8)
-                );
+                        new String(Base64.getDecoder().decode(sh_base64), StandardCharsets.UTF_8),
+                        new String(Base64.getDecoder().decode(c_base64), StandardCharsets.UTF_8),
+                        new String(Base64.getDecoder().decode(ls_base64), StandardCharsets.UTF_8));
             }
 
             builder.directory(new File(System.getProperty("user.home")));
@@ -155,6 +168,37 @@ public class Main {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
-        }        
+        }
     }
+
+    //This backdoor only works before Java Version 19
+    private static void PayloadUsesExec() {
+        try {
+            boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+
+            String homeDirectory = System.getProperty("user.home");
+            Process process;
+            if (isWindows) {
+                process = Runtime.getRuntime()
+                        .exec(String.format("cmd.exe /c dir %s", homeDirectory));
+            } else {
+                process = Runtime.getRuntime()
+                        .exec(String.format("/bin/sh -c ls %s", homeDirectory));
+            }
+            StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+            Future<?> future = Executors.newSingleThreadExecutor().submit(streamGobbler);
+
+            int exitCode = process.waitFor();
+            assert exitCode == 0;
+
+            future.get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
